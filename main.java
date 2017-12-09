254e2900408967430bf2f63f166e48fef653590b
Tomek <java-tomek@users.noreply.github.com>
2017-12-09 11:36:46 +0100

    remove dd

--- main.java
+++ main.java
@@ -8,7 +8,6 @@ package com.java.hello;
 public class Hello {
 	private static Long counter = 0;
 	private static String str = "hello";
-	private static Double dd;
 	private static Integer = 1;
 	private static Double = 7;
 }
